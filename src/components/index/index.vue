7d442654530b682f6b03a74418b9cd29bac2de44
Leesssssssss <772592971@qq.com>
2019-03-01 20:00:32 +0800

    订单管理

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -38,12 +38,17 @@
         <div class="title">用户管理</div>
         <el-table
           :data="userData"
-          border
           style="width: 100%"
           :header-cell-style="{'font-weight':700}"
         >
-          <el-table-column prop="id" label="_id" width="270" header-align="center" align="center"></el-table-column>
-          <el-table-column prop="name" label="用户名" width="220" header-align="center" align="center"></el-table-column>
+          <el-table-column prop="_id" label="_id" width="270" header-align="center" align="center"></el-table-column>
+          <el-table-column
+            prop="userName"
+            label="用户名"
+            width="220"
+            header-align="center"
+            align="center"
+          ></el-table-column>
           <el-table-column
             prop="telNum"
             label="手机号码"
@@ -70,40 +75,81 @@
       <!-- 订单管理 -->
       <el-main class="main" v-show="order">
         <div class="title">订单管理</div>
-        <el-table :data="orderData" border style="width: 100%">
+        <el-table :data="orderData" style="width: 100%">
+          <el-table-column type="expand">
+            <template slot-scope="props">
+              <el-form label-position="left" inline class="table-expand">
+                <el-form-item label="订单 ID">
+                  <span>{{ props.row._id }}</span>
+                </el-form-item>
+                <el-form-item label="订单号">
+                  <span>{{ props.row.orderNum }}</span>
+                </el-form-item>
+                <el-form-item label="收货人">
+                  <span>{{ props.row.address.name }}</span>
+                </el-form-item>
+                <el-form-item label="电话号码">
+                  <span>{{ props.row.address.telNum }}</span>
+                </el-form-item>
+                <el-form-item label="收货地址">
+                  <span>{{ props.row.address.city + props.row.address.addressDetail }}</span>
+                </el-form-item>
+                <el-form-item label="订单总价">
+                  <span>{{ props.row.price }}</span>
+                </el-form-item>
+                <div v-for="good in props.row.goods">
+                  <el-form-item label="商品名称">
+                    <span>{{ good.goodName }}</span>
+                  </el-form-item>
+                  <el-form-item label="商品数量">
+                    <span>{{ good.count }}</span>
+                  </el-form-item>
+                  <el-form-item label="商品单价">
+                    <span>{{ good.price }}</span>
+                  </el-form-item>
+                </div>
+              </el-form>
+            </template>
+          </el-table-column>
+          <el-table-column label="订单 ID" prop="_id"></el-table-column>
+          <el-table-column label="订单号" prop="orderNum"></el-table-column>
+          <el-table-column label="收货人" prop="address.name"></el-table-column>
+          <el-table-column label="电话号码" prop="address.telNum"></el-table-column>
+          <el-table-column label="操作" header-align="center" align="center">
+            <template slot-scope="scope">
+              <el-button size="mini">编辑</el-button>
+              <el-button size="mini" type="danger">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-main>
+
+      <!-- 商品管理 -->
+      <el-main class="main" v-show="good">
+        <div class="title">商品管理</div>
+        <el-table
+          :data="goodData"
+          style="width: 100%"
+          :header-cell-style="{'font-weight':700}"
+        >
+          <el-table-column prop="_id" label="_id" width="270" header-align="center" align="center"></el-table-column>
           <el-table-column
-            fixed
-            prop="id"
-            label="_id"
-            width="270"
+            prop="userName"
+            label="商品名称"
+            width="220"
             header-align="center"
             align="center"
           ></el-table-column>
-          <el-table-column
-            prop="orderNum"
-            label="订单号"
-            width="160"
-            header-align="center"
-            align="center"
-          ></el-table-column>
-          <el-table-column prop="name" label="收货人" width="220" header-align="center" align="center"></el-table-column>
           <el-table-column
             prop="telNum"
-            label="联系电话"
+            label="商品单价"
             width="220"
             header-align="center"
             align="center"
           ></el-table-column>
           <el-table-column
-            prop="address"
-            label="收货地址"
-            width="220"
-            header-align="center"
-            align="center"
-          ></el-table-column>
-          <el-table-column
-            prop="price"
-            label="订单总价"
+            prop="password"
+            label="商品分类"
             width="220"
             header-align="center"
             align="center"
@@ -121,46 +167,43 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
       user: true,
       order: false,
-      userData: [
-        {
-          id: "5bd99a8aa655750dd4879b81",
-          name: "小仙女是我",
-          telNum: "15214647650",
-          password: "123"
-        },
-        {
-          id: "5bd99a8aa655750dd4879b81",
-          name: "小仙女是我",
-          telNum: "15214647650",
-          password: "123"
-        }
-      ],
-      orderData: [
-        {
-          id: "5bd99a8aa655750dd4879b81",
-          orderNum: "1547006475008",
-          name: "鹏鹏",
-          telNum: "15214647650",
-          address: "黑龙江省哈尔滨市香坊区东北农业大学",
-          price: "35.90"
-        }
-      ]
+      good: false,
+      userData: [],
+      orderData: [],
+      goodData: []
     };
+  },
+  created() {
+    axios.get("http://localhost:7001/getUserInfo").then(res => {
+      console.log(res);
+      this.userData = res.data;
+      for (var i = 0; i < res.data.length; i++) {
+        this.orderData = this.orderData.concat(res.data[i].order);
+      }
+    });
   },
   methods: {
     handleSelect(index, indexPath) {
       if (index == 1) {
         this.user = true;
         this.order = false;
+        this.good = false;
       }
       if (index == 2) {
         this.user = false;
         this.order = true;
+        this.good = false;
+      }
+      if (index == 3) {
+        this.user = false;
+        this.order = false;
+        this.good = true;
       }
     }
   }
